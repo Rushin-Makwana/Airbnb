5fe4bc91d6a4f6bfd7aa167251ae04f9c2fd537a
Rushin-Makwana <rushymakwana@gmail.com>
2022-05-07 01:40:32 -0700

    initial Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeMode() {
+
+     mybody = document.getElementById("mylight");
+     mybody.classList.toggle('mydark');
+    // const element = document.getElementById("mylight");
+    // if (element.className == "header") {
+    //   element.className = "mydark";
+    // } else {
+    //   element.className = "mydark";
+    // }
+  }
