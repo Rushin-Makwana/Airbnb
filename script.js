a68c39a4075da4e9c2ee2307779c183c62eb5109
Rushin-Makwana <rushymakwana@gmail.com>
2022-05-10 10:45:59 -0700

    geo location add

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function changeMode() {
     //   element.className = "mydark";
     // }
   }
-document.body.onload =  userLocation ();
+ window.onload =  function (){userLocation ();}
 
 var showLocation = document.getElementById("storeLocation");
 function userLocation () {
@@ -21,8 +21,20 @@ function userLocation () {
 }
 
 function showPosition (data) {
-  // showLocation.innerHTML = "Latitude: " + position.coords.latitude +
-  // "<br> Longitude: " + position.coords.longitude;
+  // storeLocation.innerHTML = "Latitude: " + data.coords.latitude +
+  // "<br> Longitude: " + data.coords.longitude;
   console.log(data);
+let latitude = data.coords.latitude;
+let longitude = data.coords.longitude;
+
+const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${latitude}&lon=${longitude}&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`;
+
+fetch(url, {method:'GET'})
+.then((res)=>res.json())
+.then((data)=>{
+  let cityName =data.city.name
+  let weather = data.list[0].temp.day                 
+  storeLocation.innerHTML = "Weather of your City is " +weather+ "°C" 
+})
 
 }
